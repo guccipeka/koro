5a062767f61ea563074d1cdc55663ce2b747adc4
guccipeka <grand.order@icloud.com>
2022-07-13 19:12:09 +0900

    Initial Commit

---
+++ koro/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  koro
+//
+//  Created by 川口恭平 on 2022/07/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
